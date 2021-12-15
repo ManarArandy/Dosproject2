47fea67982b300eeff0bb97b8856821f7114d8f3
ManarArandi <manar.arandi@gmail.com>
2021-12-15 08:58:48 +0200

    The frontend Server Code

---
+++ Frontend.py
@@ -0,0 +1,59 @@
+from flask import Flask
+from flask_restful import Api, Resource
+import requests
+from flask_caching import Cache
+
+app = Flask(__name__)
+api = Api(app)
+#Cache
+cache = Cache(config={'CACHE_TYPE': 'SimpleCache'})
+cache.init_app(app)
+#-----------------------
+
+chacheSize = 0
+myArray = [None] * 50
+index=0
+ 
+catalogFlag = 1
+
+
+class Info(Resource):
+
+    @cache.memoize(50)
+    def get(self, num):
+        global chacheSize
+        global myArray
+        global index
+        global catalogFlag
+
+        itemfound = cache.get(str(num))
+        if itemfound:
+            return {'The item exist in the cache': itemfound},200
+        else:
+            if(catalogFlag):
+                #Server A (Windows)
+                catalogFlag = 0
+                result =requests.get('http://172.16.96.99:5000/info/'+str(num)).json() 
+                cache.set(str(num), result)
+            else:
+                #Server B (Ubuntu)
+                catalogFlag = 1
+                result =requests.get('http://172.16.96.104:5000/info/'+str(num)).json()
+                cache.set(str(num), result)
+
+        myArray[chacheSize] = str(num)
+        chacheSize = chacheSize + 1
+
+        if chacheSize> 10:
+            cache.delete(myArray[index])
+            index= index+1
+        if(catalogFlag):
+           return {'From Server B': result}
+        else:  
+           return {'From Server A': result}
+
+api.add_resource(Info, '/info/<int:num>')
+
+
+if __name__ == '__main__':
+    app.run()
