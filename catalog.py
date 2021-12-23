0268397dac92010309afbe9f303402faa374e18d
Munaayman <munaawwad09@gmail.com>
2021-12-23 18:11:47 +0200

    purchase part in catalog

--- catalog.py
+++ catalog.py
@@ -15,11 +15,10 @@ class Info(Resource):
         data_fount=self.data.loc[self.data['id'] == num]
         
         dataFrame = pd.DataFrame(data_fount, columns = ['title','amount', 'cost'])
-        res = dataFrame.to_json(orient="records")
-    
-        # return data found in csv
-        return json.loads(res),200 
-  
+        convert = dataFrame.to_json(orient="records")
+        #loads() is used to convert the JSON String document into the Python dictionary
+        parse = json.loads(convert)
+        return parse 
   
 class Search(Resource):
 
@@ -36,7 +35,33 @@ class Search(Resource):
         return parse  
 
           
-          
+class Update(Resource):
+
+    def put(self,num):
+        self.data = pd.read_csv('catalog.csv')
+        #to get book name to use it in message
+       
+        titlee = self.data.loc[self.data['id'] == num]
+        df = pd.DataFrame(titlee, columns=['title', 'amount', 'cost'])
+        title = df.iloc[0]['title']
+        
+        #get entities with the this id and decrement their amount by one after purchasing successfully
+        self.data.loc[self.data["amount"].loc[self.data["id"] == num].index,"amount"] = self.data["amount"] - 1
+        self.data.to_csv("catalog.csv", index=False)
+        
+        
+        #invalidate on frontendServer
+        requests.get('http://172.19.233.25:5000/invalidate/'+str(num))
+        #update on catalogServer on the other machine(ubunto) 
+        requests.put('http://172.19.232.86:5003/update2/item_num/'+str(num)).json()
+        return {'message from A':'You bought this book sucessfully'},200
+
+class Update2(Resource):
+
+      def put(self,num):
+        self.data = pd.read_csv('catalog.csv')
+        self.data.loc[self.data["amount"].loc[self.data["id"] == num].index,"amount"] = self.data["amount"] - 1
+        self.data.to_csv("catalog.csv", index=False)         
           
      
           
@@ -47,6 +72,7 @@ class Search(Resource):
 
 api.add_resource(Info, '/info/<int:num>')
 api.add_resource(Search, '/search/<string:topic>')
-
+api.add_resource(Update, '/update/item_num/<int:num>')
+api.add_resource(Update2, '/update2/item_num/<int:num>')
 if __name__ == '__main__':
     app.run()
